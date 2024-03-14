91fdbbf8737acec7f70c1c2508cb9c18154ef1b0
yann-dubrana <135650290+yann-dubrana@users.noreply.github.com>
2024-03-14 21:13:42 +0100

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { Map } from './components/Map.svelte';
-export { MapControl } from './components/Control';
-export { MapLayer } from './components/Layer';
-export { MapSource } from './components/Source';
